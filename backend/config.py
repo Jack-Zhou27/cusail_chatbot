26514bc08ee64311b360571f0968f3032c13a546
Jack Zhou <jkzhou2005@gmail.com>
2025-01-24 17:18:26 -0500

    idk

--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from dotenv import load_dotenv
-import os
-
-#load keys
-load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
 
