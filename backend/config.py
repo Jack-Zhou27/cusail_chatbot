822276dac6aaaf61ab07dd1342d0cefd252c9095
Jack Zhou <jkzhou2005@gmail.com>
2025-01-24 17:11:21 -0500

    fixed route

--- backend/config.py
+++ backend/config.py
@@ -4,10 +4,11 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
+#load keys
+load_dotenv()
+
 app = Flask(__name__)
 CORS(app)
 
-#load keys
-load_dotenv()
 GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
 
